eb18d8d5085ecc1281246df1e2d349bf35e8eb06
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-05-14 16:30:49 -0300

    feat: navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,39 +1,27 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import LogoCoffeeDelivery from 'public/assets/logo.svg'
 import MapPinFill from 'public/assets/icons/map-pin-fill.svg'
-// import ShoppingCartFill from 'public/assets/icons/shopping-cart-fill.svg'
+import LogoCoffeeDelivery from 'public/assets/logo.svg'
 import ShoppingCartFill from '../../../public/assets/icons/shopping-cart-fill'
-
-import 'styles/components/navbar.css'
 
 export default function Navbar() {
   return (
-    <nav className="border-4 border-rose-700 ">
-      <div className="flex justify-between mx-40 content-between">
+    <nav className="px-40 py-8">
+      <div className="flex justify-between  content-between">
         <a className="" href="#">
           <Image src={LogoCoffeeDelivery} alt="logo" width={100} height={100} />
         </a>
         <div className="flex gap-3">
-          <span
-            className="flex gap-2 items-center font-Roboto text-sm rounded-lg px-2 py-[10px] bg-brand-purple-light text-brand-purple-dark border-[1px] hover:border-brand-purple duration-300
-          "
-          >
+          <span className="flex gap-2 items-center font-Roboto text-sm rounded-lg px-2 py-[10px] bg-brand-purple-light text-brand-purple-dark border hover:border-brand-purple duration-300">
             <Image src={MapPinFill} alt="icon place" />
             Picos, PI
           </span>
           <Link
             href="/checkout"
             className="flex
-            bg-brand-yellow-light text-brand-yellow-dark rounded-lg  hover:border-brand-yellow duration-300 p-2
-            "
+            bg-brand-yellow-light text-brand-yellow-dark rounded-lg  border hover:border-brand-yellow-dark duration-300 p-2"
           >
-            {/* <Image
-              src={ShoppingCartFill}
-              alt="icon cart"
-              className="custom-icon-cart"
-            /> */}
-            <ShoppingCartFill className="custom-icon-cart w-8 h-8" />
+            <ShoppingCartFill className="w-8 h-8" />
           </Link>
         </div>
       </div>
