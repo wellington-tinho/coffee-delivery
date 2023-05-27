91f9e37e7237fbf2bf304a13860f08c70ba13961
Wellington Rodrigues <sousa_wellington@outlook.com>
2023-05-27 20:32:50 -0300

    Add new assets

--- public/assets/icons/shopping-cart-fill.tsx
+++ public/assets/icons/shopping-cart-fill.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { SVGProps, memo } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" {...props}>
