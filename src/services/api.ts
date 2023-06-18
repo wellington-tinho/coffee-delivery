10eab29c9d036d8a96af2e7207a108a425dcaeb9
wellington-tinho <sousa_wellington@outlook.com>
2023-06-17 21:27:30 -0300

    feat: create service

---
+++ src/services/api.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+let baseURL
+if (process.env.NODE_ENV !== 'production') {
+  baseURL = 'http://localhost:3333/'
+} else {
+  baseURL = 'https://api-coffee-delivery.herokuapp.com/'
+}
+
+export const api = axios.create({
+  baseURL,
+})
+
+api
+  .get('/')
+  .then((response) => console.info(response.data))
+  .catch((err) =>
+    console.warn('Não foi possivel se conectar com a API, \n\n', err),
+  )
