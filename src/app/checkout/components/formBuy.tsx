020658001f10bc616dc50148f7be1b83f8e484cc
wellington-tinho <sousa_wellington@outlook.com>
2023-07-09 17:54:38 -0300

    styles: improve styles

--- src/app/checkout/components/formBuy.tsx
+++ src/app/checkout/components/formBuy.tsx
@@ -1,10 +1,19 @@
+'use client'
+
 import CurrencyDollar from '@/../public/assets/icons/CurrencyDollar'
 import Box from '../layout/box'
 import Bank from '@/../public/assets/icons/Bank'
 import CreditCard from '@/../public/assets/icons/CreditCard'
 import Money from '@/../public/assets/icons/Money'
+import { useState } from 'react'
 
 export function FormBuy() {
+  const [payForm, setPayForm] = useState('credit-card')
+
+  function handleChangePayForm(type: string) {
+    setPayForm(type)
+  }
+
   return (
     <Box>
       <div className="flex gap-3 items-start ">
@@ -17,24 +26,48 @@ export function FormBuy() {
         </div>
       </div>
       <div className="grid grid-cols-3 gap-2 mt-4">
-        <div className="flex gap-3 items-center justify-center border  bg-base-button py-4 px-5 rounded-lg">
+        <button
+          className={` ${
+            payForm === 'debit-card'
+              ? 'border border-brand-purple bg-brand-purple-light'
+              : 'border'
+          }
+          flex gap-3 items-center justify-center border  bg-base-button py-4 px-5 rounded-lg `}
+          onClick={() => handleChangePayForm('debit-card')}
+        >
           <Bank color="#8047F8" />
           <span className="text-base-text font-normal text-sm uppercase">
             cartão de débito
           </span>
-        </div>
-        <div className="flex gap-3 items-center justify-center border border-brand-purple bg-brand-purple-light py-4 px-5 rounded-lg">
+        </button>
+        <button
+          className={` ${
+            payForm === 'credit-card'
+              ? 'border border-brand-purple bg-brand-purple-light'
+              : 'border'
+          }
+          flex gap-3 items-center justify-center border  bg-base-button py-4 px-5 rounded-lg `}
+          onClick={() => handleChangePayForm('credit-card')}
+        >
           <CreditCard color="#8047F8" />
           <span className="text-base-text font-normal text-sm uppercase">
             cartão de crédito
           </span>
-        </div>
-        <div className="flex gap-3 items-center justify-center border bg-base-button py-4 px-5 rounded-lg">
+        </button>
+        <button
+          className={` ${
+            payForm === 'money'
+              ? 'border border-brand-purple bg-brand-purple-light'
+              : 'border'
+          }
+          flex gap-3 items-center justify-center border bg-base-button py-4 px-5 rounded-lg`}
+          onClick={() => handleChangePayForm('money')}
+        >
           <Money color="#8047F8" />
           <span className="text-base-text font-normal text-sm  uppercase">
             dinheiro
           </span>
-        </div>
+        </button>
       </div>
     </Box>
   )
